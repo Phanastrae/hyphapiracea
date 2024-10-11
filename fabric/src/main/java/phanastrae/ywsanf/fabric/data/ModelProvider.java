169c0763c4c7c59834336197f99edb1993a4c8f8
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2024-10-11 03:47:33 +0100

    Setup Datagen; add lang entries and item model/texture

---
+++ fabric/src/main/java/phanastrae/ywsanf/fabric/data/ModelProvider.java
@@ -0,0 +1,28 @@
+package phanastrae.ywsanf.fabric.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.models.BlockModelGenerators;
+import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.world.item.Item;
+import phanastrae.ywsanf.item.YWSaNFItems;
+
+public class ModelProvider extends FabricModelProvider {
+    public ModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators BMG) {
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators IMG) {
+        generateFlat(IMG, YWSaNFItems.KEYED_DISC);
+    }
+
+    private static void generateFlat(ItemModelGenerators itemModelGenerator, Item item) {
+        itemModelGenerator.generateFlatItem(item, ModelTemplates.FLAT_ITEM);
+    }
+}
---
+++ fabric/src/main/java/phanastrae/ywsanf/fabric/data/YWSaNFDataGenerator.java
@@ -0,0 +1,19 @@
+package phanastrae.ywsanf.fabric.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.core.RegistrySetBuilder;
+
+public class YWSaNFDataGenerator implements DataGeneratorEntrypoint {
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModelProvider::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistrySetBuilder registryBuilder) {
+	}
+}
